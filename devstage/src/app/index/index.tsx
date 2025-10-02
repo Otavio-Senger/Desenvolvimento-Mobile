298d2c2a5a085288931119e1add9515d88f6b729
Otavio <senger.otavio@gmail.com>
2025-10-02 17:45:41 -0300

    primeira-tela

--- devstage/src/app/index/index.tsx
+++ devstage/src/app/index/index.tsx
@@ -1,3 +1,5 @@
+import { Input } from "@/components/Inputs";
+import { Mail, User } from "lucide-react-native";
 import { Image, ImageBackground, Text, View } from "react-native";
 import { styles } from "./style";
 
@@ -8,15 +10,44 @@ export default function Index() {
 				style={styles.background}
 				source={require("@/assets/Background.png")}
 			>
-                <View  style = {styles.logo}>
-                <Image source={require('@/assets/Vector.png')}/>
-                </View>
+				<View style={styles.logo}>
+					<Image
+						source={require("@/assets/Vector.png")}
+						style={styles.logoIMG}
+					/>
+				</View>
 
-            <View style={styles.texts}>
-			<Text style={styles.text1}>CodeCraft</Text>
-            <Text style={styles.text2}>Summit 2025</Text>
-            </View>
-            </ImageBackground>
+				<View style={styles.texts}>
+					<Text style={styles.text1}>CodeCraft</Text>
+					<Text style={styles.text2}>Summit 2025</Text>
+				</View>
+				{/* 
+				<View style={styles.sobreAndLogin}>
+					<View style={styles.titleAndIcon}>
+						<Text style={styles.titleSobre}>Sobre o evento</Text>
+						<View style={styles.aoVivo}>
+							<Radio size={20} color={colors.purple[300]} />
+							<Text style={styles.textAoVivo}>ao vivo</Text>
+						</View>
+					</View>
+					<Text style={styles.textSobre}>
+						Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+						criar soluções inovadoras e compartilhar conhecimento. Vamos
+						mergulhar nas tendências mais recentes em desenvolvimento de
+						software, arquitetura de sistemas e tecnologias emergentes, com
+						palestras, workshops e hackathons.
+					</Text>
+					<Text style={[styles.textSobre, { paddingTop: 16 }]}>
+						Dias 15 a 17 de março | Das 18h às 21h
+					</Text>
+				</View> */}
+
+				<View style={styles.sobreAndLogin}>
+					<Text style={styles.titleSobre}>Inscrição</Text>
+					<Input iconName={User} placeholder="Nome completo" />
+					<Input iconName={Mail} placeholder="E-mail" />
+				</View>
+			</ImageBackground>
 		</View>
 	);
 }
--- devstage/src/app/index/style.ts
+++ devstage/src/app/index/style.ts
@@ -9,9 +9,15 @@ export const styles = StyleSheet.create({
 		flex: 1,
 		width: "100%",
 	},
+	logoIMG: {
+		width: 87,
+		height: 24,
+	},
 	logo: {
-		paddingTop: 32.2,
 		alignItems: "center",
+		justifyContent: "center",
+		paddingTop: 32,
+		paddingBottom: 20,
 	},
 	text1: {
 		fontSize: 36,
@@ -25,5 +31,36 @@ export const styles = StyleSheet.create({
 	},
 	texts: {
 		alignItems: "center",
+	},
+	sobreAndLogin: {
+		backgroundColor: colors.gray[700],
+		borderRadius: 15,
+		padding: 24,
+		marginHorizontal: 16,
+		marginTop: 40,
+	},
+	titleAndIcon: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		paddingBottom: 24,
+	},
+	titleSobre: {
+		color: colors.gray[200],
+		fontSize: 20,
+		fontWeight: "600",
+	},
+	textAoVivo: {
+		fontSize: 12,
+		color: colors.purple[300],
+	},
+	aoVivo: {
+		flexDirection: "row",
+		gap: 8,
+	},
+	textSobre: {
+		color: colors.gray[300],
+		fontSize: 14,
+		fontWeight: "400",
 	},
 });
